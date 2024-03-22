b7b5884ecbfdd8cda7288864a44ef811050ed537
jose salazar <joshrs23@gmail.com>
2024-03-22 18:16:16 -0400

    new view for recover password

---
+++ pages/recover/[id]/index.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div>
+    Debugging Route2: {{ $route.params.id }}
+  </div>
+
+</template>
+
+<script>
+import { useUserStore } from "../../stores/users";
+import axios from "axios";
+ 
+export default defineComponent({
+  components: {},
+  created() {},
+  mounted() {
+
+    this.something();
+
+  },
+  data() {
+    return {
+      token: "",
+    };
+  },
+    methods: {
+        something() {
+            try{
+
+                this.token = this.$route.params.id;
+                console.log(this.token);
+
+            }catch(error){
+
+                console.log(error);
+
+            }
+        },
+
+    },
+  setup() {},
+});
+
+
+
+
+</script>
