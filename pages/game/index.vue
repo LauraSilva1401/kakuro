aa718fc9631e5a14e706012688c0e52721c19a10
Laura Silva <laura@git.com>
2024-03-22 18:52:58 -0400

    Game and home page updated

---
+++ pages/game/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="level-name">
+      <h1>{{ $route.query.level }}</h1>
+    </div>
+  </template>
+  
+
+<script>
+export default {
+  mounted() {
+    
+    const levelName = this.$route.query.level; 
+    console.log(levelName); 
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
